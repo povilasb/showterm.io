ad869ff911ac7ba362b3de445e5fd9410b67cfc5
Povilas Balciunas <balciunas90@gmail.com>
2014-06-09 19:46:18 +0300

    Updates development environment config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,11 +4,11 @@ ShowtermIo::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
-  # Show full error reports and disable caching
+  # Show full error reports and enable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -20,10 +20,10 @@ ShowtermIo::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.eager_load = false
 end
