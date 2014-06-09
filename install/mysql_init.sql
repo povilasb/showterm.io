d20192431c32e50865a7dff4cb1e5fb9603fb49a
Povilas Balciunas <balciunas90@gmail.com>
2014-06-09 20:15:02 +0300

    Fixes mysql initialization script.

--- install/mysql_init.sql
+++ install/mysql_init.sql
@@ -1,5 +1,5 @@
 create user 'showterm'@'localhost';
 
-grant all privileges on 'showterm'.* to 'showterm'@'localhost';
+grant all privileges on showterm.* to 'showterm'@'localhost';
 
 set password for 'showterm'@'localhost' = password('showterm');
